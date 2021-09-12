56a57b668f7f4307c73d725014bea533c69174ba
jennysuporn <supornthip.set@gmail.com>
2021-09-12 20:44:26 +0700

    added addItem and removeItem functionality to checkout item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from "./cart.types";
-import { addItemToCart } from "./cart.utils";
+import { addItemToCart, removeItemFromCart } from "./cart.utils";
 
 const INITIAL_STATE = {
     hidden: true,  //it's because we want to hide the cart when the user has not signed in yet
@@ -17,6 +17,19 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            };
+
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(cartItem => cartItem.id != action.payload.id ) 
+                //Filter to keep the cartItem that we're not gonna remove
             };
         default:
             return state;
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,6 +1,8 @@
 //This file is created for...
 //When user add the same item to cart, the number of item should be increase instead of showing the same item many times
 
+import CartItem from "../../components/cart-item/cart-item.component";
+
 //here, the cartItems is all the existing cart items that are in our cartItems array right now
 //here, the cartItemToAdd is the cart item that we want to add.
 //concept : we're going to look inside of our existing cart items to see if our cart item already exists.
@@ -18,6 +20,23 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     }
 
     return [...cartItems, {...cartItemToAdd, quantity: 1}] //quantity is 1 because it's the first time that this item is added
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id == cartItemToRemove.id
+    )
+
+    if (existingCartItem.quantity == 1) {   //if it has only 1, remove this item
+        return cartItems.filter(cartItem => cartItem.id != cartItemToRemove.id)
+    }
+
+    return cartItems.map (
+        cartItem => 
+        cartItem.id == cartItemToRemove.id 
+            ? { ...cartItem, quantity: cartItem.quantity - 1 }
+            : cartItem
+    )
 };
 
 
